b6854eb69b6d8923b3fd19a018cfb5738a0e4e08
wxrrry <arttema2006@gmail.com>
2026-04-23 11:53:22 +0300

    Добавлено динамическое меню

--- index.php
+++ index.php
@@ -5,15 +5,28 @@
     <title>Главная</title>
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
+<?php
+$menuItems = [
+    'Главная' => '/index.php',
+    'О нас' => '/pages/about.php',
+    'Контакты' => '/pages/contact.php'
+];
+?>
+
 <header>
     <nav>
         <ul>
-            <li><a href="/index.php">Главная</a></li>
-            <li><a href="/pages/about.php">О нас</a></li>
-            <li><a href="/pages/contact.php">Контакты</a></li>
+            <?php foreach ($menuItems as $title => $url): ?>
+                <li>
+                    <a href="<?= $url ?>" <?= $_SERVER['REQUEST_URI'] == $url ? 'class="active"' : '' ?>>
+                        <?= $title ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
 </header>
+
 
 <body>
     <?php include 'includes/header.php'; ?>
